935b9d5590f3c306a44ed7c401e4080e2b34d494
FlavioG0 <flavio.giovannangeli@gmail.com>
2014-02-14 15:29:28 +0100

    Creazione classe log

--- cl_log.py
+++ cl_log.py
@@ -2,21 +2,31 @@
 # ** FUNZIONE DI SCRITTURA LOG MENSILE SU FILE             **
 # ***********************************************************
 import time
-def WriteLog(filename,msg):
-    # Write the OPEN message to log monthly.
-    bOK = True
-    try:
-        # Monthly log
-        logTime = time.localtime(time.time())
-        month = str(time.strftime('%m', logTime))
-        year = str(time.strftime('%Y', logTime))
-        sfile = filename + '_' + month + year[2:] + '.txt'
-        flog = open(sfile,'a')
-        frtlogTime = time.strftime('%d/%m/%Y %H:%M:%S', logTime)
-        flog.write(frtlogTime + ';' + msg + '\n')
-        flog.close()
-    except IOError, error:
-        bOK = False
-        print ('LOG FILE ERROR! [' + str(error) + ']')
-    finally:
-        return bOK
+
+class log:
+
+    DEBUG = 1
+
+    def __init__(self,pathfilelog):
+        self._pathfilelog = pathfilelog
+
+
+    def write(self,description):
+        # Write the OPEN message to log monthly.
+        bOK = True
+        try:
+            # Monthly log
+            logTime = time.localtime(time.time())
+            month = str(time.strftime('%m', logTime))
+            year = str(time.strftime('%Y', logTime))
+            sfile = filename + '_' + month + year[2:] + '.txt'
+            flog = open(sfile,'a')
+            frtlogTime = time.strftime('%d/%m/%Y %H:%M:%S', logTime)
+            flog.write(frtlogTime + ';' + description + '\n')
+            flog.close()
+        except IOError, error:
+            bOK = False
+            if DEBUG == 1:
+               print ('LOG FILE ERROR! [' + str(error) + ']')
+        finally:
+            return bOK
